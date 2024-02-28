00afba0f8b1fd94e546999d82a97b8bd612fff71
antonellapineda <155715534+antonellapineda@users.noreply.github.com>
2024-02-28 21:45:51 +0800

    Add files via upload

---
+++ Solidity Basics/Midterm Lab Activity Solidity Basics II Demo/ErrorHandling.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+contract MappingExample {
+    //Define a mapping to store values associated with addressses
+    mapping (address => uint256) public valueMapping;
+
+    //Function to set a value for the sender's address
+    function setValues(uint256 _value) public {
+        require(_value !=0, "value canonot be zero");
+        valueMapping[msg.sender] = _value;
+    }
+
+    //Function to retrieve the value associated wiht the sender's address
+    function getValue() public view returns (uint256) {
+        require(valueMapping[msg.sender] !=0, "No value for sender");
+        return valueMapping[msg.sender];
+    } 
+}
